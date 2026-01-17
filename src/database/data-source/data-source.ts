c47ca857cfc1dd918ebb0fe6273337e099be8930
DESKTOP-CNK490K\Dell <phucloc.nguyen121296@gmail.com>
2026-01-17 15:54:28 +0700

    Prepare for production

--- src/database/data-source/data-source.ts
+++ src/database/data-source/data-source.ts
@@ -4,7 +4,11 @@ import { Project } from '../../modules/project/entities/project.entity';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  url: process.env.MYSQL_URL,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '3306', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  name: process.env.DB_NAME,
   entities: [User, Project],
   migrations: ['dist/database/migrations/*.js'],
   synchronize: false,
