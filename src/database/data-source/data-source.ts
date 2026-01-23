c399ae736d11298ccec1145252a7080e4f25f56f
loverforeverr93@gmail.com <loveforeverr93@gmail.com>
2026-01-23 20:24:18 +0700

    Deploy

--- src/database/data-source/data-source.ts
+++ src/database/data-source/data-source.ts
@@ -5,7 +5,7 @@ import { Project } from '../../modules/project/entities/project.entity';
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? '3306', 10),
+  port: parseInt(process.env.DB_PORT ?? '4000', 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
